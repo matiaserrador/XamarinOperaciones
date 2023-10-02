2b6a2b6e5228bdecf050b245c0ba3ebba64de232
Matias Errador <loco.matu@hotmail.com>
2023-10-02 09:55:35 -0300

    Actualización añadiendo multiplicación y división

--- XamarinFormsApp/MainPage.xaml.cs
+++ XamarinFormsApp/MainPage.xaml.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace XamarinFormsApp
+namespace XamarinOperaciones
 {
     public partial class MainPage : ContentPage
     {
@@ -14,7 +14,7 @@ namespace XamarinFormsApp
         {
             InitializeComponent();
         }
-        private void Button_Click(Object sender, EventArgs e)
+        private void BotonSUMA(Object sender, EventArgs e)
         {
             if (!String.IsNullOrEmpty(Num1.Text) && ! string.IsNullOrEmpty(Num2.Text))
             {
@@ -39,13 +39,63 @@ namespace XamarinFormsApp
                 DisplayAlert("Error", "no se han puesto los números","Ok");
             }
         }
-        private void Button_Click2(Object sender, EventArgs e)
+        private void BotonRESTA(Object sender, EventArgs e)
         {
             if (!String.IsNullOrEmpty(Num1.Text) && !string.IsNullOrEmpty(Num2.Text))
             {
                 var num1 = double.Parse(Num1.Text);
                 var num2 = double.Parse(Num2.Text);
                 var total = num1 - num2;
+                Total.Text = total.ToString();
+                string mensaje = "";
+
+                if (total > 0)
+                {
+                    mensaje = "Num Positivo++";
+                }
+                else
+                {
+                    mensaje = "Num Negativo--";
+                }
+                DisplayAlert("El resultado es", mensaje, "...");
+            }
+            else
+            {
+                DisplayAlert("Error", "no se han puesto los números", "Ok");
+            }
+        }
+        private void BotonMULTIPLICAR(Object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(Num1.Text) && !string.IsNullOrEmpty(Num2.Text))
+            {
+                var num1 = double.Parse(Num1.Text);
+                var num2 = double.Parse(Num2.Text);
+                var total = num1 * num2;
+                Total.Text = total.ToString();
+                string mensaje = "";
+
+                if (total > 0)
+                {
+                    mensaje = "Num Positivo++";
+                }
+                else
+                {
+                    mensaje = "Num Negativo--";
+                }
+                DisplayAlert("El resultado es", mensaje, "...");
+            }
+            else
+            {
+                DisplayAlert("Error", "no se han puesto los números", "Ok");
+            }
+        }
+        private void BotonDIVIDIR(Object sender, EventArgs e)
+        {
+            if (!String.IsNullOrEmpty(Num1.Text) && !string.IsNullOrEmpty(Num2.Text))
+            {
+                var num1 = double.Parse(Num1.Text);
+                var num2 = double.Parse(Num2.Text);
+                var total = num1 / num2;
                 Total.Text = total.ToString();
                 string mensaje = "";
 
